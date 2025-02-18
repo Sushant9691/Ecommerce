0510b9ef348ce5f465de4c728da3913317f1a7de
sushant45 <176743423+Sushant9691@users.noreply.github.com>
2025-02-18 19:44:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(
     credentials: true,
   })
 );
-    },
+    
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
